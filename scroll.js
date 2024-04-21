7c5854ecaf3a70e99b6168f1be5fadd8a62f3b75
Pasindu <pasindumadushan.org@gmail.com>
2024-04-21 17:48:11 +0530

    two

--- scroll.js
+++ scroll.js
@@ -10,5 +10,5 @@ ScrollReveal().reveal('.title', { delay: 500, origin: 'bottom' });
 ScrollReveal().reveal('.titleab', { delay: 500 });
 ScrollReveal().reveal('.img', { delay: 500, origin: 'left' });
 ScrollReveal().reveal('.dis', { delay: 500, origin: 'right' });
-ScrollReveal().reveal('footer', { delay: 500, origin: 'bottom' });
+// ScrollReveal().reveal('footer', { delay: 500, origin: 'bottom' });
 
